78eab52357a257413b9e71f9758cf34552956eae
Dmitry Ponyatov <dponyatov@gmail.com>
2023-09-05 00:01:41 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -1,7 +1,14 @@
 # https://kubasejdak.com/how-to-cross-compile-for-embedded-with-cmake-like-a-champ
 
-set(TARGET_TRIPLET              arm-none-eabi        )
+set(TARGET_TRIPLET              arm-none-eabi            )
 
-set(CMAKE_C_COMPILER            ${TARGET_TRIPLET}-gcc)
-set(CMAKE_CXX_COMPILER          ${TARGET_TRIPLET}-g++)
+set(CMAKE_C_COMPILER            ${TARGET_TRIPLET}-gcc    )
+set(CMAKE_CXX_COMPILER          ${TARGET_TRIPLET}-g++    )
+set(CMAKE_ASM_COMPILER          ${TARGET_TRIPLET}-as     )
+set(CMAKE_LINKER                ${TARGET_TRIPLET}-ld     )
 
+set(CMAKE_AR                    ${TARGET_TRIPLET}-ar     )
+set(CMAKE_OBJCOPY               ${TARGET_TRIPLET}-objcopy)
+set(CMAKE_RANLIB                ${TARGET_TRIPLET}-ranlib )
+set(CMAKE_SIZE                  ${TARGET_TRIPLET}-size   )
+set(CMAKE_STRIP                 ${TARGET_TRIPLET}-strip  )
