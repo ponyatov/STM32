628c98ee0eddd8cb009770872a74e25192b1b8b5
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-15 01:30:08 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/doc.mk
+++ mk/doc.mk
@@ -1,7 +1,11 @@
 # doc
-.PHONY: doc
-doc:
-
 .PHONY: doxy
 doxy: .doxygen doc/DoxygenLayout.xml doc/logo.png
 	rm -rf doc/html ; doxygen $< 1>/dev/null
+
+.PHONY: doc
+doc: \
+	doc/STM32/um1718-stm32cubemx-for-stm32-configuration-and-initialization-c-code-generation-stmicroelectronics.pdf
+
+doc/STM32/um1718-stm32cubemx-for-stm32-configuration-and-initialization-c-code-generation-stmicroelectronics.pdf:
+	$(CURL) $@ https://www.st.com/resource/en/user_manual/um1718-stm32cubemx-for-stm32-configuration-and-initialization-c-code-generation-stmicroelectronics.pdf
